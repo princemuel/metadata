87b49cf20991cb28a3a167be1aa36c0662dd6424
Prince Muel <vansomecsam@gmail.com>
2023-05-28 03:23:39 +0100

    feat: add heading component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/heading.tsx
@@ -0,0 +1,19 @@
+import { cx } from 'cva';
+
+interface Props {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
+  className?: string;
+}
+
+const Heading = ({ title, subtitle, center, className }: Props) => {
+  return (
+    <div className={cx(className, center ? 'text-center' : 'text-start')}>
+      <h2 className='text-2xl font-bold'>{title}</h2>
+      <p className='mt-2 font-light text-neutral-500'>{subtitle}</p>
+    </div>
+  );
+};
+
+export { Heading };
