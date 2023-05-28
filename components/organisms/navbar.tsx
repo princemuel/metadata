21e592fd0321bf7f4394d57239413c18d64af65e
Prince Muel <vansomecsam@gmail.com>
2023-05-28 03:22:04 +0100

    deps: add next-ath, rhform, reach hot toast
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -8,7 +8,7 @@ interface Props {}
 export function Navbar(props: Props) {
   return (
     <div className='fixed z-10 w-full bg-white shadow-sm'>
-      <div className='border-b-[1px] py-4'>
+      <div className='border-b py-4'>
         <Container>
           <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
             <Logo />
