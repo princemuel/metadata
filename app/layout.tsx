2be060810ce3a3eb681d87e8b28c29a49a0e698e
Prince Muel <vansomecsam@gmail.com>
2023-05-31 02:24:05 +0100

    feat: update root layout component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { ClientOnly, Navbar, RegisterForm } from '@/components';
+import { LoginForm } from '@/components/organisms/login';
 import { ToastProvider } from '@/lib';
 import { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import * as React from 'react';
+import getCurrentUser from './actions/get-current-user';
 import './globals.css';
 
 const font = Nunito({ subsets: ['latin'] });
@@ -12,20 +14,22 @@ export const metadata: Metadata = {
   description: 'Airbnb Clone',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getCurrentUser();
   return (
     <html lang='en'>
       <body className={font.className}>
         <React.Fragment>
           <ClientOnly>
             <ToastProvider />
+            <LoginForm />
             <RegisterForm />
           </ClientOnly>
-          <Navbar />
+          <Navbar currentUser={user} />
           {children}
         </React.Fragment>
       </body>
