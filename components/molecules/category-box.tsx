a85286a18cea5bb44d68eace0ca4b0a2eae87487
Prince Muel <vansomecsam@gmail.com>
2023-06-01 16:28:37 +0100

    feat: add category box component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/molecules/category-box.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { cx } from "cva";
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+import { useCallback } from "react";
+import { IconType } from "react-icons";
+
+interface Props {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+export const CategoryBox = ({ icon: Icon, label, selected }: Props) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {} satisfies Record<string, any>;
+    if (params) currentQuery = qs.parse(params.toString());
+
+    const updatedQuery: Record<string, any> = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className={cx(
+        "flex cursor-pointer flex-col items-center justify-center gap-2 border-b-2 p-3 transition hover:text-neutral-800",
+        selected ? "border-b-neutral-800" : "border-transparent",
+        selected ? "text-neutral-800" : "text-neutral-500"
+      )}
+    >
+      <Icon size={24} />
+      <p className="text-sm font-medium">{label}</p>
+    </div>
+  );
+};
