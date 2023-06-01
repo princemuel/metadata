87839f0d49ce3b23c974f31c5669c1a0ede58cd8
Prince Muel <vansomecsam@gmail.com>
2023-06-01 16:35:07 +0100

    feat: add categories component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,2 +1,5 @@
-export * from './navbar';
-export * from './register';
+export * from "./categories";
+export * from "./login";
+export * from "./navbar";
+export * from "./register";
+export * from "./rent-form";
