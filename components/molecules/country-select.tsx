5d155ba4add1b77fb777e40a187c1c12bedebef0
Prince Muel <vansomecsam@gmail.com>
2023-06-01 16:31:43 +0100

    feat: add countries select component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/molecules/country-select.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useCountries } from "@/lib";
+import Select from "react-select";
+
+interface Props {
+  value?: ICountry;
+  onChange: (value?: ICountry | null) => void;
+}
+
+const CountryMenu = ({ value, onChange }: Props) => {
+  const [fetchAll] = useCountries();
+
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={fetchAll()}
+        value={value}
+        onChange={(value) => onChange(value)}
+        formatOptionLabel={(option) => (
+          <div className="flex items-center gap-3">
+            <div>{option.flag}</div>
+            <p>
+              <span>{option.name}</span>,{" "}
+              <span className="ml-1 text-neutral-500">{option.region}</span>
+            </p>
+          </div>
+        )}
+        classNames={{
+          control: () => "p-3 border-2",
+          input: () => "text-lg",
+          option: () => "text-lg",
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: "black",
+            primary25: "#ffe4e6",
+          },
+        })}
+      />
+    </div>
+  );
+};
+
+export { CountryMenu };
