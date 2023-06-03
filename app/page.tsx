ccf99fc932a48ad8dd904df3b5a4eccfa1811830
Prince Muel <vansomecsam@gmail.com>
2023-06-03 06:14:43 +0100

    feat: update homepage route
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div className='text-rose-500 text-2xl'>Hello Airbnb</div>;
+import { ClientOnly, Container, EmptyState, ListingCard } from "@/components";
+import getCurrentUser from "./actions/get-current-user";
+import getListings, { IListingsParams } from "./actions/get-listings";
+
+interface HomeProps {
+  searchParams: IListingsParams;
 }
+
+const Page = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const user = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <Container>
+      <ClientOnly>
+        <ul className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {listings.map((listing) => (
+            <ListingCard user={user} key={listing.id} data={listing} />
+          ))}
+        </ul>
+      </ClientOnly>
+    </Container>
+  );
+};
+
+export default Page;
