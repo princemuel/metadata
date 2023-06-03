5084ef29aa571f39bcc1c2e40f8998d9e7626207
Prince Muel <vansomecsam@gmail.com>
2023-06-03 06:09:23 +0100

    feat: add heart button component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/heart.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import useFavorite from "@/lib/hooks/use-favorite";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface Props {
+  listing: string;
+  user?: SafeUser | null;
+}
+
+const HeartButton = ({ listing, user }: Props) => {
+  const [isFavorite, toggleFavorite] = useFavorite(listing, user);
+
+  return (
+    <button
+      type="button"
+      onClick={toggleFavorite}
+      className="relative transition hover:opacity-80"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="absolute -right-[2px] -top-[2px] fill-white"
+      />
+      <AiFillHeart
+        size={24}
+        className={isFavorite ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </button>
+  );
+};
+
+export { HeartButton };
