4a6262c4e1b1371009d7c3720e9a10b9c1700989
Prince Muel <vansomecsam@gmail.com>
2023-06-04 02:27:44 +0100

    chore: cleanup map component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/map.tsx
+++ components/molecules/map.tsx
@@ -1,31 +1,27 @@
-import Leaflet from "leaflet";
-import dynamic from "next/dynamic";
-import { useMemo } from "react";
-import * as ReactLeaflet from "react-leaflet";
-
-// Set default sizing to control aspect ratio which will scale responsively
-// but also help avoid layout shifts
-const DEFAULT_WIDTH = 600;
-const DEFAULT_HEIGHT = 600;
+import { DEFAULT_MAP_HEIGHT, DEFAULT_MAP_WIDTH } from '@/lib';
+import Leaflet from 'leaflet';
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
+import * as ReactLeaflet from 'react-leaflet';
 
 // @ts-expect-error
 interface Props extends ReactLeaflet.MapContainerProps {
   width: string | number;
   height: string | number;
   children: (f1: typeof ReactLeaflet, f2: typeof Leaflet) => React.ReactNode;
-  location: ICountry;
+  location: RentFormData['location'];
 }
 
 const Map = (props: Props) => {
   const {
     location,
-    width = DEFAULT_WIDTH,
-    height = DEFAULT_HEIGHT,
+    width = DEFAULT_MAP_WIDTH,
+    height = DEFAULT_MAP_HEIGHT,
     ...rest
   } = props;
 
   const DynamicMap = useMemo(() => {
-    return dynamic(() => import("../atoms/base-map"), {
+    return dynamic(() => import('../atoms/base-map'), {
       ssr: false,
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
