64f095d4b6e6aae4761c802e48d9e35a69009f9b
Prince Muel <vansomecsam@gmail.com>
2023-06-04 02:25:33 +0100

    style: format base map file with prettier
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/base-map.tsx
+++ components/atoms/base-map.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { cx } from "cva";
-import Leaflet from "leaflet";
-import "leaflet/dist/leaflet.css";
-import { useEffect } from "react";
-import * as ReactLeaflet from "react-leaflet";
+import { cx } from 'cva';
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
+import * as ReactLeaflet from 'react-leaflet';
 
 const { MapContainer } = ReactLeaflet;
 
@@ -19,23 +19,21 @@ const Map = ({ children, className, ...rest }: Props) => {
       //@ts-expect-error
       delete Leaflet.Icon.Default.prototype._getIconUrl;
       Leaflet.Icon.Default.mergeOptions({
-        iconRetinaUrl: "leaflet/images/marker-icon-2x.png",
-        iconUrl: "leaflet/images/marker-icon.png",
-        shadowUrl: "leaflet/images/marker-shadow.png",
+        iconRetinaUrl: 'leaflet/images/marker-icon-2x.png',
+        iconUrl: 'leaflet/images/marker-icon.png',
+        shadowUrl: 'leaflet/images/marker-shadow.png',
       });
     })();
   }, []);
 
   return (
-    <MapContainer className={cx("h-[35vh] rounded-lg", className)} {...rest}>
+    <MapContainer
+      className={cx('h-[35vh] rounded-lg', className)}
+      {...rest}
+    >
       {children?.(ReactLeaflet, Leaflet)}
     </MapContainer>
   );
 };
 
 export default Map;
-
-{
-  /* <TileLayer url={url} attribution={attribution} />
-{center ? <Marker position={center} /> : null} */
-}
