9a2218cfeba7367ef34db25f81443a78d4a710cc
Prince Muel <vansomecsam@gmail.com>
2023-06-04 02:29:23 +0100

    chore: update country menu code
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/country-menu.tsx
+++ components/molecules/country-menu.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { useCountries } from "@/lib";
-import Select from "react-select";
+import { useCountries } from '@/lib';
+import Select from 'react-select';
 
 interface Props {
-  value?: ICountry;
+  value: RentFormData['location'];
   onChange: (value?: ICountry | null) => void;
 }
 
@@ -14,32 +14,32 @@ const CountryMenu = ({ value, onChange }: Props) => {
   return (
     <div>
       <Select
-        placeholder="Anywhere"
+        placeholder='Anywhere'
         isClearable
         options={fetchAll()}
         value={value}
         onChange={(value) => onChange(value)}
         formatOptionLabel={(option) => (
-          <div className="flex items-center gap-3">
+          <div className='flex items-center gap-3'>
             <div>{option.flag}</div>
             <p>
-              <span>{option.name}</span>,{" "}
-              <span className="ml-1 text-neutral-500">{option.region}</span>
+              <span>{option.name}</span>,{' '}
+              <span className='ml-1 text-neutral-500'>{option.region}</span>
             </p>
           </div>
         )}
         classNames={{
-          control: () => "p-3 border-2",
-          input: () => "text-lg",
-          option: () => "text-lg",
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg',
         }}
         theme={(theme) => ({
           ...theme,
           borderRadius: 6,
           colors: {
             ...theme.colors,
-            primary: "black",
-            primary25: "#ffe4e6",
+            primary: 'black',
+            primary25: '#ffe4e6',
           },
         })}
       />
