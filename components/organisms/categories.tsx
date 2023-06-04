7a76cbcbbe019da07e4d481361d6aa329d57cdab
Prince Muel <vansomecsam@gmail.com>
2023-06-04 02:28:02 +0100

    chore: cleanup categories component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/categories.tsx
+++ components/organisms/categories.tsx
@@ -1,101 +1,9 @@
 "use client";
 
+import { categories } from "@/lib";
 import { usePathname, useSearchParams } from "next/navigation";
-import { BsSnow } from "react-icons/bs";
-import { FaSkiing } from "react-icons/fa";
-import {
-  GiBarn,
-  GiBoatFishing,
-  GiCactus,
-  GiCastle,
-  GiCaveEntrance,
-  GiForestCamp,
-  GiIsland,
-  GiWindmill,
-} from "react-icons/gi";
-import { IoDiamond } from "react-icons/io5";
-import { MdOutlineVilla } from "react-icons/md";
-import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import { Container } from "../atoms";
 import { CategoryBox } from "../molecules";
-
-export const categories = [
-  {
-    label: "Beach",
-    icon: TbBeach,
-    description: "This property is close to the beach!",
-  },
-  {
-    label: "Windmills",
-    icon: GiWindmill,
-    description: "This property is has windmills!",
-  },
-  {
-    label: "Modern",
-    icon: MdOutlineVilla,
-    description: "This property is modern!",
-  },
-  {
-    label: "Countryside",
-    icon: TbMountain,
-    description: "This property is in the countryside!",
-  },
-  {
-    label: "Pools",
-    icon: TbPool,
-    description: "This is property has a beautiful pool!",
-  },
-  {
-    label: "Islands",
-    icon: GiIsland,
-    description: "This property is on an island!",
-  },
-  {
-    label: "Lake",
-    icon: GiBoatFishing,
-    description: "This property is near a lake!",
-  },
-  {
-    label: "Skiing",
-    icon: FaSkiing,
-    description: "This property has skiing activies!",
-  },
-  {
-    label: "Castles",
-    icon: GiCastle,
-    description: "This property is an ancient castle!",
-  },
-  {
-    label: "Caves",
-    icon: GiCaveEntrance,
-    description: "This property is in a spooky cave!",
-  },
-  {
-    label: "Camping",
-    icon: GiForestCamp,
-    description: "This property offers camping activities!",
-  },
-  {
-    label: "Arctic",
-    icon: BsSnow,
-    description: "This property is in the arctic region!",
-  },
-  {
-    label: "Desert",
-    icon: GiCactus,
-    description: "This property is in the desert!",
-  },
-  {
-    label: "Barns",
-    icon: GiBarn,
-    description: "This property is in a barn!",
-  },
-  {
-    label: "Lux",
-    icon: IoDiamond,
-    description: "This property is brand new and luxurious!",
-  },
-];
 
 const Categories = () => {
   const params = useSearchParams();
@@ -103,7 +11,7 @@ const Categories = () => {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
 
-  // if (!isHomePage) return null;
+  if (!isHomePage) return null;
 
   return (
     <Container>
