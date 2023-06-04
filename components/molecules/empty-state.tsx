2450cf7b4b2deed2a6535de209da84207981e58e
Prince Muel <vansomecsam@gmail.com>
2023-06-04 02:24:51 +0100

    feat: update empty state component code
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/empty-state.tsx
+++ components/molecules/empty-state.tsx
@@ -1,31 +1,39 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { Button, Heading } from "../atoms";
+import { useRouter } from 'next/navigation';
+import { Button, Heading } from '../atoms';
 
 interface Props {
   title?: string;
   subtitle?: string;
   showReset?: boolean;
+  reset?: () => void;
 }
 
 const EmptyState = ({
-  title = "No exact matches",
-  subtitle = "Try changing or removing some of your filters.",
+  title = 'No exact matches',
+  subtitle = 'Try changing or removing some of your filters.',
   showReset,
+  reset,
 }: Props) => {
   const router = useRouter();
 
   return (
-    <div className="flex h-[60vh] flex-col items-center justify-center gap-2">
-      <Heading center title={title} subtitle={subtitle} />
+    <div className='flex h-[60vh] flex-col items-center justify-center gap-2'>
+      <Heading
+        center
+        title={title}
+        subtitle={subtitle}
+      />
 
-      <div className="mt-4 w-48">
+      <div className='mt-4 w-48'>
         {showReset && (
           <Button
             outline
-            label="Remove all filters"
-            onClick={() => router.push("/")}
+            label={reset ? 'Try Again' : 'Remove all filters'}
+            onClick={() => {
+              reset?.() || router.push('/');
+            }}
           />
         )}
       </div>
